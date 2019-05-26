cd9de4b947cd0a4cd867c9c8473c2fdd0a3933e3
julianaibiapina <julianibiapina@alu.ufc.br>
2019-05-26 12:10:55 -0300

    perceptron simples

---
+++ Trabalho 02/redePerceptronSimples.py
@@ -0,0 +1,55 @@
+import math
+from random import uniform
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+
+# Função y() que retorna 1 ou 0 dependendo se u >0 ou u<=0
+def func_y(ax):
+    if ax>0:
+        return 1
+    elif ax<0 or u==0:
+        return 0
+
+
+# Entrada: todas as possibilidade de x_1 e X_2
+x_1 = np.array([0, 0, 1, 1])
+x_2 = np.array([0, 1, 0, 1])
+# Saída: o resultado esperado
+d = np.array([0, 1, 1, 1])
+
+# passo de aprendizagem
+n= 0.5
+
+# cria vetor de pesos e limiar
+w = np.zeros((2,3))
+
+cont = 0 # conta as iterações
+u = 0    # amazena os produtos ecalares de casa iteração
+aux = np.empty(3)
+y = np.zeros(d.shape[0])
+
+controle = False # controle de laço
+while controle == False:
+    for i in range(0, x_1.shape[0]):
+
+        print('Entradas: \nx_1: %d e x_2: %d' % (x_1[i], x_2[i]))
+        # vetor de entrada x
+        x = np.array([-1, x_1[i], x_2[i]])
+
+        u = np.dot(x.transpose(), w[cont]) # retorna um escalar
+        y[i] = func_y(u)
+        e = d[i] - y[i]
+        #soma os elementos de posições correspondentes(axis=0 -> linha com linha)
+        aux = np.sum((w[cont], np.dot((n * e), x) ), axis=0)
+        w = np.vstack((w,aux))
+        print(w[cont])
+        cont +=1
+    # o laço 'while' só para quando o vetor 'y' for igual ao vetor esperado 'd'
+    comp = 0
+    for aux1, aux2 in itertools.zip_longest(d, y):
+        if (aux1 == aux2):
+            comp = comp + 1
+    if comp == d.shape[0]:
+        print(y)
+        controle = True
