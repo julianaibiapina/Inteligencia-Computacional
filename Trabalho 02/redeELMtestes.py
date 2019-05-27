98ff3894fa865d6d522c7592e56caad2ba22d6c8
julianaibiapina <julianibiapina@alu.ufc.br>
2019-05-27 19:28:44 -0300

    prevenção

--- Trabalho 02/redeELMtestes.py
+++ Trabalho 02/redeELMtestes.py
@@ -10,12 +10,19 @@ ones = np.ones(x_treino.shape)
 x_treino = np.hstack((ones, x_treino)) # acrescenta uma coluna de 1's
 
 y_treino = data[0:, 1:]  # potência
-
+y_treino = y_treino.reshape((1, 2250)) # transforma em um vetor de N colunas
 
 # vetor de pesos da camada oculta
-col1 = np.array([[random.uniform(0, 1)], [random.uniform(0, 1)], [random.uniform(0, 1)]])
-col2 = np.array([[random.uniform(0, 1)], [random.uniform(0, 1)], [random.uniform(0, 1)]])
+col1 = np.array([[random.uniform(0, 1)], [random.uniform(0, 1)]])
+col2 = np.array([[random.uniform(0, 1)], [random.uniform(0, 1)]])
 col = np.hstack((col1, col2))
 W = np.array(col) # matriz de pesos
 
-#cálculo das ativações dos neurônios da camada oculta
+# ativações dos neurônios da camada oculta
+Z = np.array([[], []])  # define um vetor vazio (2,1)
+for i in range(0, x_treino.shape[0]):
+    u = np.dot(W, x_treino[i]) # ativação do neurônio i
+    z = np.array([[1 / (1 + (math.exp(-1 * u[0])))], [1 / (1 + (math.exp(-1 * u[1])))]])
+    Z = np.hstack((Z,z))
+
+# peso do neurônio da camada de saída
