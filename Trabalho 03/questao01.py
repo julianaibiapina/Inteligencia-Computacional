db660b467010beb9f6187f269a2e6a1c2b7a58f1
julianaibiapina <julianibiapina@alu.ufc.br>
2019-06-07 23:00:58 -0300

    questão 01 concluída

--- Trabalho 03/questao01.py
+++ Trabalho 03/questao01.py
@@ -7,17 +7,66 @@ from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
+
+
+
 #------------------------GRÁFICO------------------------
-data = np.loadtxt('./Trabalho 03/twomoons.dat')
-x_1 = data[0:, 0:1]
-x_2 = data[0:, 1:2]
-rotulo = data[0:, 2:3]
+data_plot = np.loadtxt('./Trabalho 03/twomoons.dat')
+x_1Plot = data_plot[0:, 0:1]
+x_2Plot = data_plot[0:, 1:2]
+rotuloPlot = data_plot[0:, 2:3]
 # CLASSE A = 0:501
 # CLASSE B = 501:
+
+#----------------------TREINAMENTO----------------------
+# Embaralha os dados para o treinamento
+data = np.random.permutation(data_plot)
+x_treino = data[0:, 0:2]
+x_treino = x_treino.transpose() # transforma linhas em colunas
+ones = np.ones(x_treino.shape[1])
+x_treino = np.vstack((ones, x_treino)) # empilha o vetor de 1's em cima do vetor de entradas
+
+rotulo = data[0:, 2:3]
+rotulo = rotulo.transpose() # transforma em um vetor colunas
+
+# q = quantidade de neurônios
+q = 20
+# p = quantidade de variáveis de entrada
+p = 2
+
+# vetor de pesos da camada oculta
+W = np.random.rand(q, p+1) * 0.1
+
+
+# ativações dos neurônios da camada oculta
+u = np.matmul(W, x_treino)
+
+# Vetor Z de saídas da camada oculta
+Z = np.empty(u.shape)
+for i in range(0, u.shape[0]):
+    for j in range(0, u.shape[1]):
+        Z[i,j] = 1 / (1 + math.exp(-1 * u[i, j]))
+
+# saídas da camada oculta acrescentada com uma linha de 1's
+Z = np.vstack((ones, Z))
+
+# pesos da camada de saída
+aux1 = np.matmul(rotulo, Z.transpose())
+aux2 = np.linalg.inv(np.matmul(Z, Z.transpose()))
+M = np.matmul(aux1, aux2)
+
+# Vetor de saídas calculadas pelo modelo
+Y = np.matmul(M, Z)
+
+
+
 # CLASSE A
-ax.scatter(x_1[0:501], x_2[0:501], rotulo[0:501], c='r', marker='o')
-# CLASSE A
-ax.scatter(x_1[501:], x_2[501:], rotulo[501:], c='b', marker='^')
+ax.scatter(x_1Plot[0:501], x_2Plot[0:501], rotuloPlot[0:501], c='r', marker='o')
+# CLASSE B
+ax.scatter(x_1Plot[501:], x_2Plot[501:], rotuloPlot[501:], c='b', marker='^')
+#Classificação da Rede ELM
+ax.scatter(data[0:, 0:1], data[0:, 1:2], Y[0:], c='g', marker='^')
+
 
 ax.set_xlabel('x1')
 ax.set_ylabel('x2')
--- Trabalho 03/teste.py
+++ Trabalho 03/teste.py
@@ -19,36 +19,70 @@
 
 
 #-----------------------------------------------------------
+#
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+#
+# def randrange(n, vmin, vmax):
+#     '''
+#     Helper function to make an array of random numbers having shape (n, )
+#     with each number distributed Uniform(vmin, vmax).
+#     '''
+#     return (vmax - vmin)*np.random.rand(n) + vmin
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+#
+# n = 100
+#
+# # For each set of style and range settings, plot n random points in the box
+# # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+# for c, m, zlow, zhigh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
+#     xs = randrange(n, 23, 32)
+#     ys = randrange(n, 0, 100)
+#     zs = randrange(n, zlow, zhigh)
+#
+# ax.scatter(xs, ys, zs, c='r', marker='o')
+#
+#
+# ax.set_xlabel('X Label')
+# ax.set_ylabel('Y Label')
+# ax.set_zlabel('Z Label')
+#
+# plt.show()
+
+
+#-----------------------------------------------------------
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 
 
-def randrange(n, vmin, vmax):
-    '''
-    Helper function to make an array of random numbers having shape (n, )
-    with each number distributed Uniform(vmin, vmax).
-    '''
-    return (vmax - vmin)*np.random.rand(n) + vmin
-
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.gca(projection='3d')
 
-n = 100
+# Make data.
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
 
-# For each set of style and range settings, plot n random points in the box
-# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
-for c, m, zlow, zhigh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
-    xs = randrange(n, 23, 32)
-    ys = randrange(n, 0, 100)
-    zs = randrange(n, zlow, zhigh)
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
 
-ax.scatter(xs, ys, zs, c='r', marker='o')
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
-
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
 
 plt.show()
