7002e4c55c91f69c84ddb2ea29ab7e2d6d652208
julianaibiapina <julianibiapina@alu.ufc.br>
2019-06-09 01:57:49 -0300

    k-means completo

--- Trabalho 03/k-means.py
+++ Trabalho 03/k-means.py
@@ -4,6 +4,7 @@ import math
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 # Set three centers, the model should predict similar results
 center_1 = np.array([1,1])
@@ -18,5 +19,44 @@ data_3 = np.random.randn(200,2) + center_3
 data = np.concatenate((data_1, data_2, data_3), axis = 0)
 
 
-plt.scatter(data[:,0], data[:,1], s=7)
+
+
+# Number of clusters
+k = 3
+# Number of training data
+n = data.shape[0]
+# Number of features in the data
+c = data.shape[1]
+
+
+# Generate random centers, here we use sigma and mean to ensure it represent the whole data
+mean = np.mean(data, axis = 0)
+std = np.std(data, axis = 0)
+centers = np.random.randn(k,c)*std + mean
+
+centers_old = np.zeros(centers.shape) # to store old centers
+centers_new = deepcopy(centers) # Store new centers
+
+clusters = np.zeros(n)
+distances = np.zeros((n,k))
+
+error = np.linalg.norm(centers_new - centers_old)
+
+# When, after an update, the estimate of that center stays the same, exit loop
+while error != 0:
+    # Measure the distance to every center
+    for i in range(k):
+        distances[:,i] = np.linalg.norm(data - centers[i], axis=1)
+    # Assign all training data to closest center
+    clusters = np.argmin(distances, axis = 1)
+
+    centers_old = deepcopy(centers_new)
+    # Calculate mean for every cluster and update the center
+    for i in range(k):
+        centers_new[i] = np.mean(data[clusters == i], axis=0)
+    error = np.linalg.norm(centers_new - centers_old)
+print(centers_new)
+
+plt.scatter(data[:,0], data[:,1], s = 7)
+plt.scatter(centers_new[:,0], centers_new[:,1], c='r', s = 30,  marker='*')
 plt.show()
